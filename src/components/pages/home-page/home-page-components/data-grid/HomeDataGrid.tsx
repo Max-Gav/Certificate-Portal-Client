204bfb7153d438980d4e6d85cb4f5312f9c55c68
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-14 10:28:33 +0300

    Moved dialog component to datagrid component and refactored the datagrid component

--- src/components/pages/home-page/home-page-components/data-grid/HomeDataGrid.tsx
+++ src/components/pages/home-page/home-page-components/data-grid/HomeDataGrid.tsx
@@ -1,76 +1,106 @@
-import React from "react";
-import { DataGrid, GridDensity } from "@mui/x-data-grid";
 import { CircularProgress, Container } from "@mui/material";
+import { DataGrid, GridDensity } from "@mui/x-data-grid";
+import React, { useState } from "react";
+import { CertificateDialogProps } from "../../../../../common/types/Dialog Types/DialogProps";
 import useGetCertificates from "../../../../../hooks/queries/certificates/useGetCertificates";
+import AddCertificateDialog from "../../../../common/certificates-dialogs/Add Certificate Dialog/AddCertificateDialog";
+import CreateCertificateDialog from "../../../../common/certificates-dialogs/Create Certificate Dialog/CreateCertificateDialog";
+import EditCertificateDialog from "../../../../common/certificates-dialogs/Edit Certificate Dialog/EditCertificateDialog";
 import DataGridToolbar from "./data-grid-components/data-grid-toolbar/DataGridToolbar";
-import columns from "./data-grid-utils/dataGridColumns";
-import rowParser from "./data-grid-utils/dataGridRowParser";
-import hebrewLocaleText from "./data-grid-utils/dataGridLocaleText";
 import DataGridPagination from "./data-grid-components/DataGridPagination";
+import columns from "./data-grid-utils/dataGridColumns";
+import hebrewLocaleText from "./data-grid-utils/dataGridLocaleText";
 import onRowSelection from "./data-grid-utils/dataGridOnRowSelection";
+import rowParser from "./data-grid-utils/dataGridRowParser";
+import DialogName from "../../../../../common/types/Dialog Types/DialogName";
+import { useCurrentDialog, useIsDialogOpen } from "../../../../../hooks/context/selected-certificate-row/useDialogManager";
+
+const dialogComponentMap: Record<
+  DialogName,
+  React.FC<CertificateDialogProps>
+> = {
+  None: () => <></>,
+  Create: CreateCertificateDialog,
+  Add: AddCertificateDialog,
+  Edit: EditCertificateDialog,
+  Delete: () => <></>
+};
 
 const HomeDataGrid: React.FC = () => {
   const { data, isLoading } = useGetCertificates();
-  const [selectedRowId, setSelectedRowId] = React.useState<string>("false");
-  const [extendTools, setExtendTools] = React.useState<Boolean>(false);
-  const [density, setDensity] = React.useState<GridDensity>("standard");
+  const {currentDialog, setCurrentDialog} = useCurrentDialog()
+  const {isDialogOpen, setDialogOpen} = useIsDialogOpen()
+  const [selectedRowId, setSelectedRowId] = useState<string>("");
+  const [extendTools, setExtendTools] = useState<boolean>(false);
+  const [density, setDensity] = useState<GridDensity>("standard");
   const rows = rowParser(data);
+  const DialogComponent = dialogComponentMap[currentDialog];
 
   return (
-    <Container
-      maxWidth="xl"
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "20px",
-      }}
-    >
-      {isLoading && <CircularProgress />}
-      {data && (
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          slots={{ toolbar: DataGridToolbar, pagination: DataGridPagination }}
-          slotProps={{ toolbar: { extendTools } }}
-          initialState={{
-            density: "standard",
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
+    <>
+      <DialogComponent
+        certificateId={selectedRowId}
+        open={isDialogOpen}
+        onClose={() => {
+          setDialogOpen(false);
+          setCurrentDialog("None");
+        }}
+      />
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        {isLoading && <CircularProgress />}
+        {data && (
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            slots={{ toolbar: DataGridToolbar, pagination: DataGridPagination }}
+            slotProps={{ toolbar: { extendTools } }}
+            initialState={{
+              density: "standard",
+              pagination: {
+                paginationModel: {
+                  pageSize: 5,
+                },
               },
-            },
-          }}
-          autoHeight={true}
-          showCellVerticalBorder={true}
-          onDensityChange={(newDensity) => setDensity(newDensity)}
-          density={density}
-          localeText={hebrewLocaleText}
-          pageSizeOptions={[5, 10, 15]}
-          disableMultipleRowSelection={true}
-          onRowSelectionModelChange={(rowSelectionModel) => {
-            onRowSelection(
-              rowSelectionModel,
-              setSelectedRowId,
-              setExtendTools
-            );
-          }}
-          className="elevated-element"
-          sx={{
-            borderRadius: "15px",
-            backgroundColor: "white",
+            }}
+            autoHeight={true}
+            showCellVerticalBorder={true}
+            onDensityChange={(newDensity) => setDensity(newDensity)}
+            density={density}
+            localeText={hebrewLocaleText}
+            pageSizeOptions={[5, 10, 15]}
+            disableMultipleRowSelection={true}
+            onRowSelectionModelChange={(rowSelectionModel) => {
+              onRowSelection(
+                rowSelectionModel,
+                setSelectedRowId,
+                setExtendTools
+              );
+            }}
+            className="elevated-element"
+            sx={{
+              borderRadius: "15px",
+              backgroundColor: "white",
 
-            //  Remove Column Resizing
-            ".MuiDataGrid-columnSeparator": {
-              display: "none",
-            },
-            "&.MuiDataGrid-root": {
-              border: "none",
-            },
-          }}
-        />
-      )}
-    </Container>
+              //  Remove Column Resizing
+              ".MuiDataGrid-columnSeparator": {
+                display: "none",
+              },
+              "&.MuiDataGrid-root": {
+                border: "none",
+              },
+            }}
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
