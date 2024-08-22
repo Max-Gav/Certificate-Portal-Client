5b49f590fe16604a156d42d41d70431b91e3e7fc
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-22 11:38:09 +0300

    Added upload certificate API route

--- src/config/api/APIRoutes.ts
+++ src/config/api/APIRoutes.ts
@@ -1,6 +1,7 @@
 export default{
     GET_CERTIFICATES: '/certificate/get',
     CREATE_CERTIFICATE: '/certificate/create',
+    UPLOAD_CERTIFICATE: '/certificate/upload',
     EDIT_CERTIFICATE: '/certificate/edit',
     DELETE_CERTIFICATE: '/certificate/delete',
     LOGIN: '/users/login',
