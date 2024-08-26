1c3a57380412fe4ffea8e0c86b30244eb897063e
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-26 10:54:30 +0300

    Fixed import and removed unnecessary code

--- src/components/pages/home-page/HomePage.tsx
+++ src/components/pages/home-page/HomePage.tsx
@@ -2,21 +2,13 @@ import React from "react";
 import HomeNavbar from "./home-page-components/HomeNavbar";
 import HomeDataGrid from "./home-page-components/data-grid/HomeDataGrid";
 import ProtectedPage from "../../common/protected-page/ProtectedPage";
-import DialogManagerProvider from "../../../hooks/context/selected-certificate-row/DialogManagerProvider";
+import DialogManagerProvider from "../../../hooks/context/dialog-manager/DialogManagerProvider";
 
 const HomePage: React.FC = () => {
   return (
     <>
       <ProtectedPage isTokenRequired={true}>
         <HomeNavbar />
-        {/* <Box sx={{ display: "flex", justifyContent: "center", gap: 2 }}>
-        <Link href="/register" variant="body2">
-          <Button variant="contained">{"יצירת משתמש"} </Button>
-        </Link>
-        <Link href="/login" variant="body2">
-          <Button variant="contained">{"התחברות"} </Button>
-        </Link>
-      </Box> */}
         <DialogManagerProvider>
           <HomeDataGrid />
         </DialogManagerProvider>
